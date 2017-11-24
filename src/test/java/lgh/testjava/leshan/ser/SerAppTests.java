2eb01d460468697ed683844be63e7977ff945d30
JohnsonLau <gatement@gmail.com>
2017-11-24 18:22:48 +0800

    go2

---
+++ src/test/java/lgh/testjava/leshan/ser/SerAppTests.java
@@ -0,0 +1,16 @@
+package lgh.testjava.leshan.ser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SerAppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
